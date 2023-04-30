2b1335de45a312d2fe53a20a292db315b2ecb656
1804 <rugision.yuki@yandex.com>
2023-04-30 22:59:18 +0800

    day22

--- graduation_design_pcmw/src/main/java/indi/gd/pcmw/service/AdministratorService.java
+++ graduation_design_pcmw/src/main/java/indi/gd/pcmw/service/AdministratorService.java
@@ -3,12 +3,13 @@ package indi.gd.pcmw.service;
 import indi.gd.pcmw.domain.Hospital;
 import indi.gd.pcmw.domain.User;
 
-import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 public interface AdministratorService {
     int adminLoginValidate(User user);
 
     List<Hospital> getAllHospitals();
+
+
 
 }
