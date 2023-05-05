7a04ad7e7d8823b037fbb56eaab70c26c2b2e3cf
1804 <rugision.yuki@yandex.com>
2023-05-05 23:00:44 +0800

    day24

---
+++ graduation_design_pcmw/src/main/java/indi/gd/pcmw/config/RedisConfig.java
@@ -0,0 +1,23 @@
+package indi.gd.pcmw.config;
+
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig extends CachingConfigurerSupport {
+
+    @Bean
+    public RedisTemplate<Object, Object> redisTemplate (RedisConnectionFactory redisConnectionFactory){
+        RedisTemplate<Object, Object> template = new RedisTemplate<>();
+        //更改key序列化器为StringRedisSerializer
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+
+        template.setConnectionFactory(redisConnectionFactory);
+        return template;
+    }
+}
--- graduation_design_pcmw/src/test/java/indi/gd/pcmw/GraduationDesignPcmwApplicationTests.java
+++ graduation_design_pcmw/src/test/java/indi/gd/pcmw/GraduationDesignPcmwApplicationTests.java
@@ -2,14 +2,25 @@ package indi.gd.pcmw;
 
 import indi.gd.pcmw.dto.ApplyDTO;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations;
+
+import java.util.List;
+import java.util.Set;
 
 @SpringBootTest
 class GraduationDesignPcmwApplicationTests {
-
+    @Autowired
+    private RedisTemplate<Object, Object> redisTemplate;
     @Test
     void contextLoads() {
     }
+    @Test
+    public void simpleTest(){
+        redisTemplate.opsForValue().set("addr","tw");
 
 
+    }
 }
